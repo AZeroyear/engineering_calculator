fc48e243ebd714c6f8cad299be7f7b664b4b2f00
Azeroyear <eastazy@gmail.com>
2018-11-24 08:13:37 +0900

    update

--- lib/engineering_calculator.rb
+++ lib/engineering_calculator.rb
@@ -1,4 +1,5 @@
 require "engineering_calculator/version"
+require 'yaml'
 
 module EngineeringCalculator
   class Calculator
@@ -14,11 +15,12 @@ module EngineeringCalculator
       result.reject!{ |c| c.empty? }
 
       result.map! do |elem|
-        elem.sub!(/^\^/, "**")
-        elem =~ (/(^\d+)/)
-        [$1||elem, nil, $1, $']
+        original = split_unit(elem)
+        conv = split_unit(conversion(elem))
+        [conv["value"], conv["unit"], original["value"], original["unit"]]
       end
-      #calc(result)
+      formula = String.new
+      calc(result)
     end
 
     def calc(result)
@@ -26,5 +28,40 @@ module EngineeringCalculator
       result.each { |elem| formula << elem[0] }
       eval(formula)
     end
+
+    def conversion(keisan)
+      #SI単位を返す
+      vu = split_unit(keisan)
+
+      all_unit = YAML.load_file("lib/unit.yml")
+      all_unit.each_value { |type| keisan = (type[vu["unit"]] * vu["value"].to_i).to_s + type.key(1) if type.key?(vu["unit"]) }
+      keisan.sub!(/^\^/, "**") if keisan =~ /^\^/
+      keisan
+    end
+
+    def unit_conversion(unit)
+
+    end
+
+
+    def split_unit(keisan)
+      keisan =~ /(^\*+\d+)|([\(\)\+\*\/])|(\d+.*\d)|(^\^\d+)/
+      value = $&
+      unit = $'
+      hash = Hash.new
+      hash = {"value" => value, "unit" => unit}
+    end
   end
 end
+
+=begin
+    def formula
+      root()
+      sqrt()
+      _/()
+
+      ^
+      **
+
+    end
+=end
